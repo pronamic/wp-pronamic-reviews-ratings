1f0158648e7b17f76b1d3ad8896f1191b6173e34
Remco Tolsma <info@remcotolsma.nl>
2014-04-09 11:40:18 +0200

    Added an comment template file.

---
+++ examples/comment-microdata.php
@@ -0,0 +1,25 @@
+<?php 
+
+// @see http://schema.org/Review
+// @see http://schema.org/Rating
+// @see https://github.com/WordPress/WordPress/blob/3.8.2/wp-includes/comment-template.php#L1742
+
+?>
+<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+	<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+		<span itemprop="name"><?php printf( __( 'Comment %d', 'text_domain' ), get_comment_ID() ); ?></span> -
+		by <span itemprop="author"><?php comment_author(); ?></span>,
+
+		<meta itemprop="datePublished" content="<?php comment_date( 'Y-m-d' ); ?>" /><?php comment_date(); ?>
+
+		<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+			<meta itemprop="worstRating" content="1" />
+			<span itemprop="ratingValue">4</span>/
+			<span itemprop="bestRating">5</span> stars
+		</div>
+		
+		<div class="comment-content" itemprop="description">
+			<?php comment_text(); ?>
+		</div>
+	</div>
+</li>
