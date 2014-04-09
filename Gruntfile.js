d732d2290c200c56b151b84641f0fffba9dc59ea
Remco Tolsma <info@remcotolsma.nl>
2014-04-09 09:49:51 +0200

    Added Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function( grunt ) {
+	// Project configuration.
+	grunt.initConfig( {
+		// Package
+		pkg: grunt.file.readJSON( 'package.json' ),
+		
+		// PHPLint
+		phplint: {
+			options: {
+				phpArgs: {
+					'-lf': null
+				}
+			},
+			all: [ '**/*.php' ]
+		},
+
+		// MakePOT
+		makepot: {
+			target: {
+				options: {
+					cwd: '',
+					domainPath: 'languages',
+					type: 'wp-plugin'
+				}
+			}
+		}
+	} );
+
+	grunt.loadNpmTasks( 'grunt-phplint' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+
+	// Default task(s).
+	grunt.registerTask( 'default', [ 'phplint' ] );
+	grunt.registerTask( 'pot', [ 'makepot' ] );
+};
